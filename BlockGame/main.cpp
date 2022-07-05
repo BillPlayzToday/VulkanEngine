de44080d57c77dfa13de85b982b33a3e3b78c391
BillPlayzToday <tonileoels@gmail.com>
2022-07-05 13:47:10 +0200

    Shader Compiling + Shader modules

--- BlockGame/main.cpp
+++ BlockGame/main.cpp
@@ -10,6 +10,7 @@
 #include <optional>
 #include <set>
 #include <algorithm>
+#include <fstream>
 
 
 const uint32_t WIDTH = 800;
@@ -352,7 +353,28 @@ private:
 	}
 
 	void createGraphicsPipeline() {
-		// TODO
+		auto vertShaderCode = readFile("shaders/vert.spv");
+		auto fragShaderCode = readFile("shaders/frag.spv");
+
+		VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+		VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+		VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+		vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+		vertShaderStageInfo.module = vertShaderModule;
+		vertShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+		fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+		fragShaderStageInfo.module = fragShaderModule;
+		fragShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+		vkDestroyShaderModule(device, vertShaderModule, nullptr);
+		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 	}
 
 
@@ -449,6 +471,24 @@ private:
 		return VK_FALSE;
 	}
 
+	static std::vector<char> readFile(const std::string& filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+
+		return buffer;
+	}
+
 
 	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
 		QueueFamilyIndices indices;
@@ -539,6 +579,20 @@ private:
 
 			return actualExtent;
 		}
+	}
+
+	VkShaderModule createShaderModule(const std::vector<char>& code) {
+		VkShaderModuleCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+		VkShaderModule shaderModule;
+		if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+			throw std::runtime_error("can't create shader module!");
+		}
+
+		return shaderModule;
 	}
 
 	std::vector<const char*> getRequiredExtensions() {
---
+++ BlockGame/shaders/compile.bat
@@ -0,0 +1,3 @@
+C:/Users/tonil/Desktop/BlockGame/Libraries/VulkanSDK/1.3.216.0/Bin/glslc.exe shader.vert -o vert.spv
+C:/Users/tonil/Desktop/BlockGame/Libraries/VulkanSDK/1.3.216.0/Bin/glslc.exe shader.frag -o frag.spv
+pause
